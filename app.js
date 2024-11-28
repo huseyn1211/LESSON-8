52598bc8b9d20e661ffc6787f1566b885752f6c5
huseyn1211 <quluzadeh43@gmail.com>
2024-11-28 18:49:24 +0400

    homework

---
+++ app.js
@@ -0,0 +1,26 @@
+let a = 99
+let b = 0
+while (a > 0) {
+
+   a -= 2
+   b += a
+}
+console.log(b)
+
+
+// let numb = prompt("reqem daxil edin");
+// let main = 100
+//  let dif = 0
+// while(main<1000)
+// {
+//    main++
+   
+//    if(numb % main ==0){
+//       dif+=main
+//       console.log(dif)
+      
+      
+//    }
+
+// //   console.log(dif)
+// }
